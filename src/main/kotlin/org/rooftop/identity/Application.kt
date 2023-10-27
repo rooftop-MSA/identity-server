756879efd9551a31403ffcf39ad2ca4d2e0a95a5
xb205 <62425964+devxb@users.noreply.github.com>
2023-10-27 19:20:46 +0900

    build: 프로젝트 세팅 (#1)

---
+++ src/main/kotlin/org/rooftop/identity/Application.kt
@@ -0,0 +1,15 @@
+package org.rooftop.identity
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Application {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplication.run(Application::class.java, *args)
+        }
+    }
+}
